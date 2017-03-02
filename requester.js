52e57e6a9af17b0286007db1d480fbbf143438d6
ShaTiNik <rexpower@tut.by>
2017-03-02 22:26:42 +0300

    [Misc] Добавлены комментарии к коду

--- requester.js
+++ requester.js
@@ -11,17 +11,24 @@
 const requests = require('./configs/requests');
 
 module.exports = {
+    /**
+    * Функция преобразования полей(получения значений) типа 'variable' и 'request' в 'static'.
+    * Замена "путей к значениям" на сами значения.
+    */
     loadConditions: function (req, res, next, title, where, callback) {
         let static = {};
         let subquery = false;
         let subquery_field = '';
+
         for (let field in where) {
             let value = where[field].value;
             let type = where[field].type;
+
             switch (type) {
                 case 'var':
                 case 'variable':
                     if (req.query[value]) {
+                        // Преобразование объекта 'variable' в 'static'
                         static[field] = { type: 'static', value: req.query[value] };
                     } else {
                         next(); // Error::NotEnoughData
@@ -36,15 +43,20 @@ module.exports = {
                     if (!subquery) {
                         let _subquery = where[field];
                         if (_subquery.field) {
+
+                            // Параметр rows создаётся только после выполнения подзапроса
                             if (_subquery.rows) {
                                 let count = _subquery.rows.length;
                                 let values = [];
                                 for (let i = 0; i < count; i++) {
+                                    // Формирование массива из значений поля, которое нужно получить
                                     values[i] = _subquery.rows[i][_subquery.field];
                                 }
                                 if (values.count == 1) {
                                     values = values[0];
                                 }
+
+                                // Преобразование объекта типа 'query' в 'static'
                                 static[field] = { type: 'static', value: values };
                                 break;
                             } else {
@@ -61,6 +73,7 @@ module.exports = {
                     break;
             }
         }
+
         if (subquery) {
             if (this.getConfig(subquery.value)) {
                 this.query(subquery.value, req, res, next, function (req, res, next, rows) {
@@ -75,6 +88,7 @@ module.exports = {
         }
         return static;
     },
+
     query: function (title, req, res, next, callback, _w) {
         let request = this.getConfig(title);
         let table = request.table;
@@ -83,13 +97,17 @@ module.exports = {
         let fields = request.fields;
         let query = knex(table);
 
+        // Совмещение исходных полей where с данными, переданными через параметр "_w"
         if (request.where) {
             where = Object.assign(request.where);
         }
         if (where) {
             where = Object.assign(where, _w);
         }
+
         let static = this.loadConditions(req, res, next, title, where, callback);
+
+        // Static принимает значение false в случае, если запрос содержит подзапросы, либо в случае возникновения ошибки в процессе обработки запроса
         if (static) {
             switch (type) {
                 case 'select':
@@ -107,6 +125,7 @@ module.exports = {
                 case 'delete':
                     break;
             }
+            // Применение параметров where к запросу
             for (let field in static) {
                 let value = static[field].value;
                 if (value instanceof Array) {
@@ -115,6 +134,7 @@ module.exports = {
                     query.where(field, value);
                 }
             }
+
             if (callback) {
                 query.then(function (rows) {
                     callback(req, res, next, rows);
@@ -127,6 +147,7 @@ module.exports = {
             }
         }
     },
+
     getConfig: function (title) {
         for (let i = 0; i < requests.length; i++) {
             if (requests[i].title == title) {
