d9a3f634d755c803524f667fa44a5a731e1546c5
ShaTiNik <rexpower@tut.by>
2017-05-17 11:02:49 +0300

    [Общее] Изменен тип взаимодействия внутри express
    Восстановлен оригинальный тип взаимодействия, соответствующий структуре express

--- _resmi/events.js
+++ _resmi/events.js
@@ -1,20 +1,27 @@
-module.exports = {
+events = {
+    accessDenied: function (req, res) {
+        res.send('access denied');
+    },
+    
     /*
     * CanWeDid-function. Return false to stop the execution.
     */
-    before: function(req, res, next) {
-
-        let somethingWentWrong = false;
+    before: function (req, res, next) {
+        var somethingWentWrong = false;
 
         if (somethingWentWrong) {
-            return false;
+            events.accessDenied(req, res);
+            return;
         }
     
         next(); // go to handler
     },
 
-    after: function(req, res) {
+    after: function (req, res, next) {
         // Don't care what you'll do here. Answer was already send.
         // Maybe it's unusuable feature
-    },
-}
+        next();
+    }
+};
+
+module.exports = events;
--- _resmi/index.js
+++ _resmi/index.js
@@ -1,75 +1,35 @@
 const express = require('express');
 const log = require('logger');
 const routes = require('../configs/routes');
-const serverStartType = process.env.NODE_ENV || 'development';
 const events = require('./events');
 
 global.service = 'videosos';
 
-function construct(handler, action, route) {
-    let handle = require(`../handlers/${handler}`)[action];
-
-    return function (req, res, next) {
-        let _if = changelevel(req, res, next, route);
-        
-        _if(events.before) (function(){
-            _if(handle) (function(){
-                // I could write _if(events.after)(next)
-                // But I don't care what the function returns and it'll just exec the next code
-                events.after(req, res);
-                next();
-            });
-        });
-    }
-}
-
-function changelevel(req, res, next, route) {
-    return function (from) {
-        return function (to) {
-            _changelevel(req, res, next, from, to, route);
-        }
-    }
-}
-
-function _changelevel(req, res, next, from, to, route) {
-    let result = from(req, res, to, route);
-
-    if (result === true) {
-        // all OK. Answer has been send
-    } else if (result === undefined) {
-        // no info. Processing is going to deep levels. nothing to do.
-        // Answer might be already sended. Not sure.
-    } else if (result === false) {
-        // bad news. Something going wrong and we can't process it.
-        next(); // looks like it'll be a 404-page
-    }
-}
-
 module.exports = function(port) {
     var app = express();
-
     app.listen(port, function () {
-        log.info('system', `Server is running on port ${port} in ${serverStartType} mode.`);
+        log.info('system', `Server is running on port ${port} in ${app.get('env')} mode.`);
     });
-
+    app.use(events.before);
     for (let i in routes) {
         let route = routes[i];
-        
+        let handle = require(`../handlers/${route.handler}`)[route.action];
         switch (route.method) {
             case 'get':
-                app.get(route.uri, construct(route.handler, route.action, route));
+                app.get(route.uri, handle);
                 break;
             case 'post':
-                app.post(route.uri, construct(route.handler, route.action, route));
+                app.post(route.uri, handle);
                 break;
             case 'put':
-                app.put(route.uri, construct(route.handler, route.action, route));
+                app.put(route.uri, handle);
                 break;
             case 'delete':
-                app.delete(route.uri, construct(route.handler, route.action, route));
+                app.delete(route.uri, handle);
                 break;
             default:
                 log.warn('router', `Route(id: ${i}) have an unexpectable method`);
         }
     }
+    app.use(events.after);
 }
