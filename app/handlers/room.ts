4dd8af5587c79d6ab38fc82b1981902f384950f1
ShaTiNik <rexpower@tut.by>
2017-07-30 23:12:20 +0300

    [Feature] Implementation of typeorm with example in handler

--- app/handlers/room.ts
+++ app/handlers/room.ts
@@ -1,9 +1,15 @@
+import { connect } from '../../resmi/database/typeorm'
+import { Room } from '../entity/Room'
+
 export namespace roomHandler {
     export function getInfo(req, res, next) {
-      res.json({
-        "kind": process.env.service + '#' + 'room' + 'getInfo' + 'Response',
-        "items": []
-      });
-      next();
+      connect.then(async connection => {
+        let roomRepository = connection.getRepository(Room);
+        res.json({
+          "kind": process.env.service + '#' + 'room' + 'getInfo' + 'Response',
+          "items": await roomRepository.find()
+        });
+        next();
+      })
     }
 }
