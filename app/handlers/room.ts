cfd5354546c5ea35e8e9c42432afc6bb42aeec37
ShaTiNik <rexpower@tut.by>
2017-07-31 12:03:47 +0300

    [Misc] Added error messages to Room Handler

--- app/handlers/room.ts
+++ app/handlers/room.ts
@@ -1,14 +1,32 @@
 import { connect } from '../../resmi/database/typeorm'
 import { Room } from '../entity/Room'
+import { Connection } from 'typeorm';
+import { Packet } from '../../resmi/services/Packet';
+import { Request, Response, NextFunction } from 'express'
 
 export namespace roomHandler {
-    export function getInfo(req, res, next) {
+    export function getInfo(req: Request, res: Response, next: NextFunction) {
       connect.then(async connection => {
-        let roomRepository = connection.getRepository(Room);
-        res.json({
-          "kind": process.env.service + '#' + 'room' + 'getInfo' + 'Response',
-          "items": await roomRepository.find()
-        });
+        let packet = new Packet('room', 'getInfo');
+        if (req.query.id) {
+          let id: number = Number(req.query.id);
+          if (!isNaN(id)) {
+            if (connection instanceof Connection && connection.isConnected) {
+              let roomRepository = connection.getRepository(Room);
+              packet.first = await roomRepository.findOneById(id);
+              if (!packet.first) {
+                packet.error = `No room with ID ${id}`
+              }
+            } else {
+              packet.error = 'DBConnection error';
+            }
+          } else {
+            packet.error = 'ID is NaN';
+          }
+        } else {
+          packet.error = 'ID is empty';
+        }
+        res.json(packet);
         next();
       })
     }
