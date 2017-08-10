ed72bee543b01142f8b6da033e3851c72b775c32
ShaTiNik <rexpower@tut.by>
2017-08-10 21:56:33 +0300

    [Misc] Added Packet-class

--- src/packet.ts
+++ src/packet.ts
@@ -1,6 +1,65 @@
-export class Packet {
+class ResponseTemplate {
+    kind: string;
+    item: any;
+    items: any[];
+    error?: string;
+}
 
-    constructor() {
-        
+export default class Packet {
+    private _items: any[];
+
+    private handler: string;
+    private action: string;
+    private _error: string;
+
+    constructor(handler: string, action: string) {
+        this.handler = handler;
+        this.action = action;
+    }
+
+    get error(): string {
+        return this._error;
+    }
+
+    set error(text: string) {
+        this._error = text;
+    }
+
+    get items(): any[] {
+        return this._items;
+    }
+
+    set items(items: any[]) {
+        this._items = items;
+    }
+
+    set first(item: any) {
+        this._items = [item];
+    }
+
+    get first(): any {
+        return this._items[0];
+    }
+
+    get isEmpty(): boolean {
+        return !Boolean(this._items);
+    }
+
+    toJSON(): object {
+        let res: ResponseTemplate = new ResponseTemplate();
+        res.kind = `${process.env.service}#${this.handler}${this.action}Response`;
+        if (!this.isEmpty) {
+            if (this.items.length == 1) {
+                res.item = this.items[0];
+            } else {
+                res.items = this.items;
+            }
+        }
+        if (this.error) {
+            res.error = this.error;
+        } else {
+            res.error = undefined;
+        }
+        return res;
     }
 }
