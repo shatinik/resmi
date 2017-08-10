09983dc258b20a6d93216bdb46ec473cb9779ea0
ShaTiNik <rexpower@tut.by>
2017-08-10 20:06:51 +0300

    [General] Cleaup

--- configs/cron.d.ts
+++
@@ -1,9 +0,0 @@
-declare interface CronEvent {
-  title: string
-  pattern: string
-  start: boolean
-  timeZone: string
-  runs: () => void
-  stops: () => void
-  onComplete: () => void
-}
--- configs/production/mysql.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host: process.env.db_host,
-    user: process.env.db_user,
-    password: process.env.db_password,
-    database: process.env.db_database,
-    charset: process.env.db_charset
-};
