bc24e8426a69decb3a569f8a0ed511a026b4b866
ShaTiNik <rexpower@tut.by>
2017-08-10 21:58:06 +0300

    [Misc] Route-logic and Handler improvements

--- src/handler.ts
+++ src/handler.ts
@@ -1,10 +1,19 @@
+import { Request, Response, NextFunction  } from 'express'
+
 export class Handler {
+
+    protected static obj: Handler;
 
     protected beforeAction() {
 
     }
 
-    constructor() {
+    protected constructor() {}
 
+    public static run(req: Request, res: Response, next: NextFunction, action: string) {
+        if (!this.obj) {
+            this.obj = new this();
+        }
+        this.obj[action](req, res, next);
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,37 @@
 import * as express  from 'express';
 import log from './logger'
 
-declare interface route {
-    method: string,
-    uri: string,
-    handler: string,
-    action: string
+class Route {
+    private _method: string;
+    private _uri: string;
+    private _handler: string;
+    private _action: string;
+
+    public set method(val: string) { this._method = val.toLowerCase() }
+    public set uri(val: string) { this._uri = val.toLowerCase() }
+    public set handler(val: string) { this._handler = val.toLowerCase() }
+    public set action(val: string) { this._action = val }
+
+    public get method(): string { return this._method }
+    public get uri(): string { return this._uri }
+    public get handler(): string { return this._handler }
+    public get action(): string { return this._action }
+
+    public get filename(): string { return `./handlers/${this.handler}/${this.handler}.${this.method}` }
+    public get className(): string { return this.handler.charAt(0).toUpperCase() + this.handler.slice(1).toLowerCase() + this.method.charAt(0).toUpperCase() + this.method.slice(1).toLowerCase() }
+
+    constructor(data) {
+        Object.assign(this, data);
+    }
 }
-const routes: route[] = require('../configs/routes');
+
+const routes: Route[] = require('../configs/routes');
 
 export default class Routes {
 
     public static load(app: express.Application): void {
         for (let i in routes) {
-            let route = routes[i];
+            let route = new Route(routes[i]);
             let action = Routes.buildAction(route);
             switch (route.method) {
                 case 'get':
@@ -34,19 +52,16 @@ export default class Routes {
         }
     }
 
-    private static buildAction(route: route) {
-        let fileName = `./handlers/${route.handler}/${route.handler}.${route.method}`;
-        let handlerName = Routes.toCamelCase(route.handler, route.method);
-        let actionName = route.action;
+    private static buildAction(route: Route) {
         return function (req, res, next) {
-            let handler = new (require(fileName)[handlerName])(req, res, next, actionName);
-            log.debug('router', `Call ${handlerName}::${actionName} from ${req.connection.remoteAddress}`);
+            let handler = require(route.filename)[route.className].run(req, res, next, route.action);
+            log.debug('router', `Call ${route.className}::${route.action} from ${req.connection.remoteAddress}`);
         }
     }
 
     private static toCamelCase(...args: string[]): string {
         let text = args[0];
-        for (let i = 0; i < args.length; i++) {
+        for (let i = 1; i < args.length; i++) {
             text += args[i].charAt(0).toUpperCase() + args[i].slice(1).toLowerCase();
         }
         return text;
