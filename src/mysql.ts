843c6fe117a9fe05a2ebb39c040e6be16bc029a1
ShaTiNik <rexpower@tut.by>
2017-08-10 21:56:18 +0300

    [Misc] MySQL connected

--- src/mysql.ts
+++ src/mysql.ts
@@ -1,6 +1,26 @@
-export class MySQL {
+import {createConnection} from "typeorm";
+import log from './logger'
 
-    constructor() {
-        
-    }
+interface mysqlConfig {
+    host: string
+    user: string
+    password: string
+    database: string
+    charset: string
 }
+
+let config: mysqlConfig = require(`../configs/${process.env.NODE_ENV}/mysql`);
+let connect = createConnection({
+    type: "mysql",
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'videosos',
+    entities: [
+        __dirname + "/models/*.js",
+    ],
+    autoSchemaSync: true,
+}).catch(error => log.error('typeorm', error));
+
+export default connect;
