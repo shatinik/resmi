be4d2f23c79d77ab32e8b611fc12e735820415b2
ShaTiNik <rexpower@tut.by>
2017-08-15 19:19:26 +0300

    [Misc] Playlist::addNew has been implemented

--- src/handlers/playlist/playlist.post.ts
+++ src/handlers/playlist/playlist.post.ts
@@ -1,6 +1,53 @@
-import Handler from '../../handler'
+import Handler from '../../handler';
 import { Request, Response, NextFunction  } from 'express'
+import connect from '../../mysql'
+import { Connection } from 'typeorm';
+import Room from '../../models/mysql/Room';
+import log from '../../logger'
+import Packet from '../../packet';
+import Playlist from '../../models/mysql/Playlist'
 
 export class PlaylistPost extends Handler {
-    public addNew(req, res: Response, next: NextFunction): void {}
+    public addNew(req, res: Response, next: NextFunction): void {
+        let packet = new Packet('playlist', 'addNew');
+        connect.then(async connection => {
+            if (req.query.title && req.query.preview_picture_url && req.query.room) {
+                let title: string = req.query.title;
+                let preview_picture_url: string = req.query.preview_picture_url;
+                let room_id: number = req.query.room;
+                if (title && preview_picture_url) {
+                    if (connection instanceof Connection && connection.isConnected) {
+                        let userRepository = connection.getRepository(Room);
+                        let room: Room = await userRepository.findOneById(room_id);
+                        if (room) {
+                            let roomRepository = connection.getRepository(Playlist);
+                            let playlist: Playlist = new Playlist();
+                            playlist.title = title;
+                            playlist.delete_ban = false;
+                            playlist.preview_picture_url = preview_picture_url;
+                            playlist.room = room; // WTF?
+                            roomRepository.save(playlist).then(room => {
+                                packet.first = 'Ok';
+                                res.json(packet);
+                                next();
+                            });
+                            return;
+                        }
+                    } else {
+                        log.error('typeorm', 'DBConnection error');
+                        packet.error = 'Internal error';
+                    }
+                } else {
+                    packet.error = 'ID is NaN';
+                }
+            } else {
+                packet.error = 'Not enough data';
+            }
+            if (packet.error) {
+                log.debug('net', packet.error);
+            }
+            res.json(packet);
+            next();
+        })
+    }
 }
