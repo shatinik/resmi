bf9314d4e5497f3aa57b08f1d69a7d6da93dd509
ShaTiNik <rexpower@tut.by>
2017-08-15 13:22:03 +0300

    [Misc] User model has been updated

--- src/models/mysql/User.ts
+++ src/models/mysql/User.ts
@@ -1,14 +1,36 @@
-import {Entity, Column, PrimaryGeneratedColumn} from "typeorm";
+import {Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn} from "typeorm";
 
 @Entity()
 export default class User {
 
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column("int")
-  service: number;
+    @Column("varchar")
+    name: string;
 
-  @Column("varchar")
-  service_uid: number;
+    @Column("varchar")
+    email: string;
+
+    @CreateDateColumn()
+    created_at: string;
+
+    // @Column({
+    //     type: "datetime",
+    //     default: 'CURRENT_TIMESTAMP(6)'
+    // })
+    // last_auth: string;
+
+    @Column("varchar")
+    picture_full_uri: string;
+
+    @Column("varchar")
+    picture_cut_uri: string;
+
+    @Column("int")
+    service: number;
+
+    @Column("varchar")
+    service_uid: number;
+
 }
