803bbabb6faee7666f2c3201a41c361208a2f040
ShaTiNik <rexpower@tut.by>
2017-08-31 16:57:39 +0300

    [Misc] Decorator @authorized_only now can be used only with actions

--- src/decorators.ts
+++ src/decorators.ts
@@ -2,20 +2,26 @@ import { Request, Response, NextFunction  } from 'express'
 import Packet from './packet'
 import log from './logger'
 
-export function authorized_only() {
-    return function (object: any, methodName: string, descriptor: TypedPropertyDescriptor<Function>) {
+declare interface Action {
+    (req: Request, res: Response, next: NextFunction, packet: Packet): void;
+}
+
+declare type ActionDecorator = (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<Action>) => TypedPropertyDescriptor<Action> | void;
+
+export function authorized_only(): ActionDecorator {
+    return function (object: any, methodName: string, descriptor: TypedPropertyDescriptor<Action>) {
         let action = descriptor.value;
 
-        descriptor.value = async function(req, res: Response, next: NextFunction, packet: Packet) {
-            if (req.user) {
-                await action(req, res, next, packet);
-            } else {
+        descriptor.value = async function(req: Request, res: Response, next: NextFunction, packet: Packet) {
+            if (!req.user) {
                 log.fatal('system', 'ATTENTION! Authenticate before calling room::add. Remove this message after enabling RBAC');
                 packet.error = 'Not logged in';
                 next(packet);
+            } else {
+                await action(req, res, next, packet);
             }
         };
 
         return descriptor;
-    }
+    };
 }
