05a4f5f10f7b1e62e4f2e8ee21573363967b878a
ShaTiNik <rexpower@tut.by>
2017-09-04 18:28:48 +0300

    [Misc] Reimplementation of class Packet

--- src/packet.ts
+++ src/packet.ts
@@ -1,72 +1,37 @@
-class ResponseTemplate<T> {
-    kind: string;
-    item: T;
-    items: T[];
-    token?: string;
-    error?: string;
+interface IPacket<T> {
+    kind: string
+    items: T[]
+    error?: string
+    token?: string
 }
 
-export default class Packet<T> {
-    private _items: T[];
+export default class Packet<T> implements IPacket<T> {
+    public kind: string;
+    public items: T[] = [];
+    public error: string;
+    public token: string;
 
-    private handler: string;
-    private action: string;
-    private _error: string;
-    private _token: string;
-
-    constructor(handler: string, action: string) {
-        this.handler = handler;
-        this.action = action;
-    }
-
-    get token(): string {
-        return this._token;
-    }
-
-    set token(value: string) {
-        this._token = value;
-    }
-
-    get error(): string {
-        return this._error;
-    }
-
-    set error(text: string) {
-        this._error = text;
-    }
-
-    get items(): T[] {
-        return this._items;
-    }
-
-    set items(items: T[]) {
-        this._items = items;
+    constructor(
+        private handler: string,
+        private action: string
+    ) {
+        this.kind = `${process.env.service}#${this.handler}${this.action}Response`;
     }
 
     set first(item: T) {
-        this._items = [item];
+        this.items = [item];
     }
 
     get first(): T {
-        return this._items[0];
+        return this.items[0];
     }
 
-    get isEmpty(): boolean {
-        return !Boolean(this._items);
-    }
-
-    toJSON(): object {
-        let res: ResponseTemplate<any> = new ResponseTemplate();
-        res.kind = `${process.env.service}#${this.handler}${this.action}Response`;
-        if (!this.isEmpty) {
-            res.items = this.items;
+    toJSON(): IPacket<T> {
+        return {
+            kind: this.kind,
+            items: this.items,
+            error: this.error,
+            token: this.token
         }
-        if (this.error) {
-            res.error = this.error;
-        }
-        if (this.token) {
-            res.token = this.token;
-        }
-        return res;
     }
 }
