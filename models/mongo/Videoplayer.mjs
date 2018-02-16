b45d0b1941e548fd779eae4454712b24b72e7aec
shatinik <rexpower@tut.by>
2018-02-17 02:04:21 +0300

    [Fix] Model Videoplayer

--- models/mongo/Videoplayer.mjs
+++ models/mongo/Videoplayer.mjs
@@ -3,6 +3,28 @@ import Room from './Room'
 
 const Schema = mongoose.Schema;
 
+var options = {
+    autoIndex: null, //bool - defaults to null (which means use the connection's autoIndex option)
+    bufferCommands: true, //bool - defaults to true
+    capped: false, //bool - defaults to false
+    collection: 'videos', //string - no default
+    emitIndexErrors: false, //bool - defaults to false.
+    id: true, //bool - defaults to true
+    _id: true, //bool - defaults to true
+    minimize: true, //bool - controls document#toObject behavior when called manually - defaults to true
+    //read: '', //string
+    safe: true, //bool - defaults to true.
+    shardKey: null, //bool - defaults to null
+    strict: true, //bool - defaults to true
+    //toJSON: {}, //- object - no default
+    //toObject: {}, //- object - no default
+    typeKey: 'type', //- string - defaults to 'type'
+    useNestedStrict: false, //- boolean - defaults to false
+    validateBeforeSave: true, //- bool - defaults to true
+    versionKey: '__v', //string - defaults to "__v"
+    collation: null, //object - defaults to null (which means use no collation)
+};
+
 let _fields = {
     Video: {
         type: Schema.Types.ObjectId,
