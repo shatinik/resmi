257e5bcf8894742e313f7d0310089f47ee5964de
shatinik <rexpower@tut.by>
2018-04-27 01:26:51 +0300

    [Misc] Remove mongoDB connection from entry-point

--- core/core.mjs
+++ core/core.mjs
@@ -1,14 +1,9 @@
 process.env.NODE_ENV = 'development';
 process.env.service = 'videosos';
 import Server        from './server';
-import Application   from '../modules/application/application';
-import log           from './logger';
-import MongoDB       from './mongodb'
 
 /*
 Здесь собирается и запускается движок
 */
-let app = new Application();
-MongoDB();
 let server = new Server("resmi.ni", 1337);
 server.start();
