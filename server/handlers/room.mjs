8aa2712d4a5b9e8318198c9e37a2eaee8e7cf834
shatinik <rexpower@tut.by>
2018-05-15 16:27:01 +0300

    One more conflict

--- server/handlers/room.mjs
+++ server/handlers/room.mjs
@@ -5,28 +5,6 @@ const room = 'name: ROOMtest'
 export default class RoomGet {
 
     constructor() {}
-<<<<<<< HEAD
-
-    mapping = async (method, params) => {
-        let success = false;
-
-        switch (method) {
-            case 'getAll':
-                success = true;
-                return await this.getAll();
-                break;
-            default:
-                if (!success) { return await this.emergency(); }
-        } 
-    }
-
-    getAll = async => {
-        return room
-    }
-
-    emergency = async => {
-        return 'Error in parameters'
-=======
 
     getAll = async (id) => {
         return room;
@@ -35,6 +13,5 @@ export default class RoomGet {
     addNote = async (room) => {
         let response = `ROOMHANDLER: ${room.name}`;
         return response;
->>>>>>> remotes/origin/handler-auto-loader
     }
 }
